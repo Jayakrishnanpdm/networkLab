7ed77d387ddf1c0cbf6711fff1ff0681091afae0
Jayakrishnanpdm <jayakrishnanmanupdm@gmail.com>
2024-07-08 23:44:04 +0530

    intial

---
+++ leaky.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+    int buck_size,incoming,outgoing,store=0,n;
+    printf("enter bucket_size,outgoing and number of inputs");
+    scanf("%d",&buck_size,&outgoing,&n);
+    while (n!=0)
+    {
+      printf("enter incoming packet size");
+      scanf("%d",&incoming);
+      if(incoming<=(buck_size - store))
+      {
+        store=store + incoming;
+        printf("bucket buffer size is %d out of %d",store,buck_size);
+        store = store-outgoing;
+        printf("After outgoing %d left out of %d",store,buck_size);
+      }
+    }
+    
+}
